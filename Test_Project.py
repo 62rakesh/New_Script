02bf36709db74a810f510d276e22267e54722738
Rakesh Patra <rakeshkumarpatra62@gmail.com>
2019-11-27 19:14:10 +0530

    commiting a new file

--- Test_Project.py
+++ Test_Project.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver: webdriver = webdriver.Chrome()
 driver.implicitly_wait(10)
+warning = ['This name already exist! ']
 
 
 def test_Setup():
@@ -48,6 +50,29 @@ def test_dev_admin_Authorizations():
     driver.find_element_by_xpath("(//INPUT[@id='name'])[2]").send_keys("Automation")
     sleep(1)
     driver.find_element_by_xpath("(//BUTTON[@class='btn btn-primary btn-sm'])['Add']").click()
+    sleep(2)
+    # if "//DIV[@class='err-msg ng-star-inserted']" == warning:
+    #     try:
+    #         driver.find_element_by_xpath("(//INPUT[@id='name'])[2]").send_keys("Exception Roles")
+    #     except:
+    #         driver.find_element_by_xpath("(//INPUT[@id='name'])[2]").send_keys("Automation")
+    # else:
+    #     return True
+
+
+def test_dev_Authorization_user():
+    driver.find_element_by_xpath(
+        "/html/body/app-root/app-admin/div/div[1]/div[2]/app-nav-menu/nav/ul/li[3]/ul/li[2]/a").click()
+    sleep(2)
+
+
+def test_hover(driver, xpath):
+    element_to_hover_over = driver.find_element_by_xpath("/html/body/app-root/app-admin/div/div[2]/app-users/div["
+                                                         "2]/div/div/div/div/mat-table/mat-row[2]/mat-cell["
+                                                         "6]/div/button")
+    hover = ActionChains(driver).move_to_element(element_to_hover_over)
+    hover.perform()
+    sleep(2)
 
 
 def test_teardown():
